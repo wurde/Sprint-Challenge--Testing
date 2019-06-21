2257d967bca53ffc2722c89955e7553dee25c18c
Andy Bettisworth <andy@accreu.com>
2019-06-21 10:48:43 -0500

    Spec the tests TODO

---
+++ server/test/routes/games_router.test.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const supertest = require('supertest')
+const app = require('../../app')
+
+/**
+ * Assertions
+ */
+
+describe('routes', () => {
+  describe('games_router.js', () => {
+    test.todo('GET /game - not found') // 404 Not Found
+    test.todo('GET /games - success')
+    test.todo('GET /games - return empty array if no games')
+    test.todo('POST /games - success')
+    test.todo('POST /games - missing request body') // 422 Unprocessable Entity
+    test.todo('POST /games - missing request body fields') // 422 Unprocessable Entity
+    test.todo('POST /games - uniqueness conflict') // 409 Conflict
+    test.todo('GET /games/:id - success')
+    test.todo('GET /games/:id - not found')
+    test.todo('PUT /games/:id - success')
+    test.todo('PUT /games/:id - not found')
+    test.todo('DELETE /games/:id - success')
+    test.todo('DELETE /games/:id - not found')
+  })
+})
